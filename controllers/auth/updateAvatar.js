f47cec3990741594c73ff2e947ccccd387101172
Svi-Kos <svitlanakos@gmail.com>
2021-09-15 17:14:27 +0300

    avatar processing using jimp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const Jimp = require("jimp");
 
 const { User } = require("../../models");
 
@@ -9,8 +10,10 @@ const updateAvatar = async (req, res) => {
   const { path: tempPath, originalname } = req.file;
   const uploadPath = path.join(avatarsDir, originalname);
   try {
+    const file = await Jimp.read(tempPath);
+    await file.resize(250, 250).write(tempPath);
     await fs.rename(tempPath, uploadPath);
-    const avatar = `/public/avatars/${originalname}`;
+    const avatar = `/avatars/${originalname}`;
     await User.findByIdAndUpdate(req.user._id, { avatarURL: avatar });
     res.json({ status: "success", code: 200, data: { avatarURL: avatar } });
   } catch (error) {
